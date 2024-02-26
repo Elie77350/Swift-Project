2f76b5366318179d36660643dcf18636835a70fc
Elie BINGUE <eliebingue7@icloud.com>
2024-02-26 18:06:58 +0100

    Initial Commit

---
+++ MiseEnPratique/MiseEnPratiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiseEnPratiqueApp.swift
+//  MiseEnPratique
+//
+//  Created by Elie BINGUE on 26/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MiseEnPratiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
